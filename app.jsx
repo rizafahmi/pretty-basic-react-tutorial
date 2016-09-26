338982f8bf02bf2338e283648a3ae23839709b69
Riza Fahmi <rizafahmi@gmail.com>
2016-09-26 16:51:45 +0700

    Static JSX element for mocking in

--- app.jsx
+++ app.jsx
@@ -1,3 +1,43 @@
+function Application() {
+  return (
+    <div className="scoreboard">
+      <div className="header">
+        <h1>Scoreboard</h1>
+      </div>
+
+      <div className="players">
+        <div className="player">
+          <div className="player-name">
+            Jim Hoskins
+          </div>
+          <div className="player-score">
+            <div className="counter">
+              <button className="counter-action decrement"> - </button>
+              <div className="counter-score"> 31 </div>
+              <button className="counter-action increment"> + </button>
+            </div>
+          </div>
+        </div>
+
+
+        <div className="player">
+          <div className="player-name">
+            Andrew Hopkins
+          </div>
+          <div className="player-score">
+            <div className="counter">
+              <button className="counter-action decrement"> - </button>
+              <div className="counter-score">65</div>
+              <button className="counter-action increment"> + </button>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
 ReactDOM.render(
-  <h1>Hello</h1>, document.getElementById('container')
+  <Application />, document.getElementById('container')
 )
