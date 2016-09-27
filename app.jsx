264829b1c490c8fc2510ab594a325d1c1ec16e3f
Riza Fahmi <rizafahmi@gmail.com>
2016-09-27 07:12:04 +0700

    Add event and using `setState` for setting a new state of the score

--- app.jsx
+++ app.jsx
@@ -52,13 +52,23 @@ var Counter = React.createClass({
       score: 0
     }
   },
+  incrementScore(e) {
+    this.setState({
+      score: this.state.score + 1
+    })
+  },
+  decrementScore(e) {
+    this.setState({
+      score: this.state.score - 1
+    })
+  },
   render() {
 
     return (
       <div className="counter">
-        <button className="counter-action decrement"> - </button>
+        <button className="counter-action decrement" onClick={this.decrementScore}> - </button>
         <div className="counter-score">{this.state.score}</div>
-        <button className="counter-action increment"> + </button>
+        <button className="counter-action increment" onClick={this.incrementScore}> + </button>
       </div>
 
     )
