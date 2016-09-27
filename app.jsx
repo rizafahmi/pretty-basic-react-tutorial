84667f4b35bcde9e83fc377484e1f65b118956b1
Riza Fahmi <rizafahmi@gmail.com>
2016-09-27 07:00:07 +0700

    Change props to state using getInitialState and returns state object like
    ```return {
      score: 0
    }```

--- app.jsx
+++ app.jsx
@@ -34,7 +34,7 @@ function Player(props) {
         {props.name}
       </div>
       <div className="player-score">
-        <Counter score={props.score} />
+        <Counter />
       </div>
     </div>
   )
@@ -46,15 +46,18 @@ Player.PropTypes = {
 }
 
 var Counter = React.createClass({
-  propTypes: {
-    score: React.PropTypes.number.isRequired
+  propTypes: {},
+  getInitialState() {
+    return {
+      score: 0
+    }
   },
   render() {
 
     return (
       <div className="counter">
         <button className="counter-action decrement"> - </button>
-        <div className="counter-score">{this.props.score}</div>
+        <div className="counter-score">{this.state.score}</div>
         <button className="counter-action increment"> + </button>
       </div>
 
