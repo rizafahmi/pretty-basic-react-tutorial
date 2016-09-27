9e3711ab9e336ba2e726a4ee72343c756a3b6430
Riza Fahmi <rizafahmi@gmail.com>
2016-09-27 17:20:06 +0700

    Add Stat component to count everything

--- app.jsx
+++ app.jsx
@@ -16,15 +16,44 @@ var PLAYERS =[
   }
 ]
 
+function Stats(props) {
+  var totalPlayers = props.players.length
+  var totalPoints = props.players.reduce((total, player) => {
+    return total + player.score
+  }, 0)
+  return (
+    <table className="stats">
+      <tbody>
+        <tr>
+          <td>Players:</td>
+          <td>{totalPlayers}</td>
+        </tr>
+        <tr>
+          <td>Total Points:</td>
+          <td>{totalPoints}</td>
+        </tr>
+      </tbody>
+    </table>
+  )
+}
+
+Stats.PropTypes = {
+  players: React.PropTypes.array.isRequeired
+}
+
+
 function Header(props) {
   return (
     <div className="header">
+      <Stats players={props.players}/>
       <h1>{ props.title }</h1>
     </div>
   )
 }
+
 Header.porpType = {
-  title: React.PropTypes.string
+  title: React.PropTypes.string,
+  players: React.PropTypes.array.isRequeired
 }
 
 function Player(props) {
@@ -94,7 +123,7 @@ var Application = React.createClass({
   render () {
     return (
       <div className="scoreboard">
-      <Header title={this.props.title} />
+      <Header title={this.props.title} players={this.state.players} />
       <div className="players">
 
       {this.state.players.map((player, index) => {
